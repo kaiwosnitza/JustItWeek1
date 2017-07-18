d64ab9bcee957a97e4c544552d2cf1cbcb6ab1c0
kaiwosnitza <kai-wosnitza@hotmail.co.uk>
2017-07-18 16:20:12 +0100

    Add files via upload

---
+++ js/myScript.js
@@ -0,0 +1,12 @@
+/*==============================================
+THIS IS A FICTIONAL WEBSITE FOR TRAINING PURPOSE
+================================================*/
+
+/*==============================================
+            MOBILE DEVICE MENU SCRIPT
+================================================*/
+
+$("span.navBtn").click(function() {
+    $("ul.menu-items").slideToggle();
+});
+
